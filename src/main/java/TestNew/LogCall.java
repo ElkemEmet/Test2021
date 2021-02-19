1e33b4d5af2e7b63ff416f0257731f02f0efd2d9
ElkemEmet <68960989+ElkemEmet@users.noreply.github.com>
2021-02-18 21:28:53 -0500

    Update LogCall.java

--- src/main/java/TestNew/LogCall.java
+++ src/main/java/TestNew/LogCall.java
@@ -3,5 +3,8 @@ package TestNew;
 public class LogCall {
     public static void main(String[] args) {
         System.out.println("calling muhtar");
+        
+        // i will see this line on intellij
+        // yes i will
     }
 }
