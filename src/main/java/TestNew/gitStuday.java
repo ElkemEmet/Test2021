74b5d27e89ba9e6dd2d7be3d95b2f1f21c2d4d4b
ElkemEmet <elkem131@gmail.com>
2021-04-09 15:20:08 -0400

    update new row

--- src/main/java/TestNew/gitStuday.java
+++ src/main/java/TestNew/gitStuday.java
@@ -3,6 +3,8 @@ package TestNew;
 public class gitStuday {
     public static void main(String[] args) {
         System.out.println("new test case");
+        System.out.println("added new again");
+        System.out.println("I want to deleted some row");
 
         // I added one line
         // I added two line
